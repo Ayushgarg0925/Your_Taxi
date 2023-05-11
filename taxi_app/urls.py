f9935b6e034f49234b61f35ab0a900ac1149de65
Ayushgarg0925 <ayushgarg0925@gmail.com>
2023-05-11 12:24:11 +0530

    home page

--- taxi_app/urls.py
+++ taxi_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.http import HttpResponse
+from .views import *
 
 def test(request):
     return HttpResponse("ok report")
 
 urlpatterns = [
-    path('', test),
+    path('a', test),
+    path('',home),
 ]
